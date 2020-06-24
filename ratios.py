8c38bc5076a2dcf62857294ae112156300190f79
Robert Wayne <robertjwayne@gmail.com>
2020-06-24 07:23:36 +0700

    Add current ratio

---
+++ ratios.py
@@ -0,0 +1,80 @@
+# # Liquidity Ratios
+
+
+# Current Ratio
+# Measures a company's ability to pay off short-term liabilities with current assets:
+# Current ratio = Current assets / Current liabilities
+def current_ratio(current_assets, current_liabilities):
+    return current_assets / current_liabilities
+
+# The acid-test ratio measures a company’s ability to pay off short-term
+# liabilities with quick assets:
+# Acid-test ratio = Current assets – Inventories / Current liabilities
+#
+# The cash ratio measures a company’s ability to pay off short-term liabilities with cash and cash equivalents:
+# Cash ratio = Cash and Cash equivalents / Current Liabilities
+#
+# The operating cash flow ratio is a measure of the number of times a company can pay off current liabilities with the cash generated in a given period:
+# Operating cash flow ratio = Operating cash flow / Current liabilities
+#
+# Leverage Financial Ratios
+
+# Leverage ratios measure the amount of capital that comes from debt. In other words, leverage financial ratios are used to evaluate a company’s debt levels. Common leverage ratios include the following:
+
+# The debt ratio measures the relative amount of a company’s assets that are provided from debt:
+# Debt ratio = Total liabilities / Total assets
+# The debt to equity ratio calculates the weight of total debt and financial liabilities against shareholders’ equity:
+# Debt to equity ratio = Total liabilities / Shareholder’s equity
+#
+# The interest coverage ratio shows how easily a company can pay its interest expenses:
+# Interest coverage ratio = Operating income / Interest expenses
+#
+# The debt service coverage ratio reveals how easily a company can pay its debt obligations:
+# Debt service coverage ratio = Operating income / Total debt service
+#
+# Efficiency Ratios
+# Efficiency ratios, also known as activity financial ratios, are used to measure how well a company is utilizing its assets and resources. Common efficiency ratios include:
+#
+# The asset turnover ratio measures a company’s ability to generate sales from assets:
+# Asset turnover ratio = Net sales / Total assets
+#
+# The inventory turnover ratio measures how many times a company’s inventory is sold and replaced over a given period:
+# Inventory turnover ratio = Cost of goods sold / Average inventory
+
+# The accounts receivable turnover ratio measures how many times a company can turn receivables into cash over a given period:
+# Receivables turnover ratio = Net credit sales / Average accounts receivable
+#
+# The days sales in inventory ratio measures the average number of days that a company holds on to inventory before selling it to customers:
+#
+# Days sales in inventory ratio = 365 days / Inventory turnover ratio
+
+# Profitability Ratios
+
+# Profitability ratios measure a company’s ability to generate income relative to revenue, balance sheet assets, operating costs, and equity. Common profitability financial ratios include the following:
+
+# The gross margin ratio compares the gross profit of a company to its net sales to show how much profit a company makes after paying its cost of goods sold:
+
+# Gross margin ratio = Gross profit / Net sales
+# The operating margin ratio compares the operating income of a company to its net sales to determine operating efficiency:
+# Operating margin ratio = Operating income / Net sales
+#
+# The return on assets ratio measures how efficiently a company is using its assets to generate profit:
+# Return on assets ratio = Net income / Total assets
+# The return on equity ratio measures how efficiently a company is using its equity to generate profit:
+#
+# Return on equity ratio = Net income / Shareholder’s equity
+#
+# Market Value Ratios
+
+# Market value ratios are used to evaluate the share price of a company’s stock. Common market value ratios include the following:
+# The book value per share ratio calculates the per-share value of a company based on equity available to shareholders:
+# Book value per share ratio = Shareholder’s equity / Total shares outstanding
+#
+# The dividend yield ratio measures the amount of dividends attributed to shareholders relative to the market value per share:
+#
+# Dividend yield ratio = Dividend per share / Share price
+# The earnings per share ratio measures the amount of net income earned for each share outstanding:
+# Earnings per share ratio = Net earnings / Total shares outstanding
+#
+# The price-earnings ratio compares a company’s share price to its earnings per share:
+# Price-earnings ratio = Share price / Earnings per share
